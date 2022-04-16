072beaa833855b3d30fe6e90aee2a73b2b31e4bf
Ohto-Ai <zhuguangfeng.thatboy@gmail.com>
2022-04-17 03:15:01 +0800

    Add test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
-#include "clipshare.h"
-#include <QtWidgets/QApplication>
+﻿#include "clipshare.h"
+#include "SingleApplication.h"
+#include <cpp-httplib/httplib.h>
+#include <ghc/filesystem.hpp>
+#include <fplus/fplus.hpp>
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    SingleApplication a(argc, argv);
+
+    spdlog::info("CLIPSHARE initializing~");
+    if (a.instanceRunning())
+    {
+        spdlog::info("Another application has running, bye~");
+        return 0;
+    }
+
+    a.setWindowIcon(QIcon{ "res/icon/main.png" });
+
     clipshare w;
     w.show();
+
+	spdlog::info("Interface crate.");
     return a.exec();
 }
