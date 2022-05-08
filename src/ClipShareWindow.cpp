8d2ca3fdae62d802f4546273ca779eb6cab985ee
OhtoAi#ФωФ <zhuguangfeng.thatboy@gmail.com>
2022-05-08 14:50:05 +0800

    Fix typo error

--- src/ClipShareWindow.cpp
+++ src/ClipShareWindow.cpp
@@ -55,8 +55,7 @@ ClipShareWindow::ClipShareWindow(QWidget *parent)
             else
             {
                 spdlog::warn("[Heartbeat] Incorrect heartbeat package size: {}", datagramData.size());
-                spdlog::warn("[Heartbeat] Incorrect heartbeat package content: {:a}", spdlog::to_hex(datagramData)););
-            }
+                spdlog::warn("[Heartbeat] Incorrect heartbeat package content: {:a}", spdlog::to_hex(datagramData));
         }
     });
 
